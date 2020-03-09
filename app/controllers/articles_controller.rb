bb2f51a1eeab0c959ee2986dadf046c9cf6ebfad
tojio <firebirdv.toji.fv5cl7@gmail.com>
2020-03-09 19:06:21 +0900

    新規論文投稿ページを作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,23 @@ class ArticlesController < ApplicationController
     @article = Article.all
   end
   
+  def new
+    @article = Article.new
+  end
 
 
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to @article
+  end
+
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title,:body)
+  end
 
 
 end
