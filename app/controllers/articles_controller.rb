a6c3b9bfec9031dc4eedbc4ba13dac4fbde7b407
tojio <firebirdv.toji.fv5cl7@gmail.com>
2020-03-09 23:44:11 +0900

    更新と削除の機能を追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,13 +11,27 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
-
   def create
     @article = Article.new(article_params)
     @article.save
     redirect_to @article
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to @article
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
 
   private
 
